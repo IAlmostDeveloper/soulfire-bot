1624b3fdad428ad971cd54ac9cd5ca81fd91ad84
developer <kill2survive@yandex.ru>
2022-02-06 17:21:00 +0500

    Update /news command output

--- src/Commands/NewsCommand.cs
+++ src/Commands/NewsCommand.cs
@@ -1,4 +1,6 @@
-﻿using Soulfire.Bot.Services;
+﻿using Soulfire.Bot.Core.InlineButtons;
+using Soulfire.Bot.Services;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,7 +22,12 @@ namespace Soulfire.Bot.Commands
         public async Task Execute(IChatService chatService, long chatId, long userId, int messageId, string commandText)
         {
             var newsList = await _service.GetTopHeadlines();
-            var buttons = newsList.Articles.ToDictionary(x => x.Title.Substring(0, 10), x => x.Description.Substring(0, 10));
+            var buttons = newsList.Articles.Select(x =>
+            {
+                return new List<InlineButton>{
+                    new InlineButton(x.Title.Substring(0, 40), x.Url, Core.Enumerations.InlineButtonType.Url) 
+                };
+            }).Take(10);
             await chatService.SendMessage(chatId, "Top news:", buttons);
         }
     }
